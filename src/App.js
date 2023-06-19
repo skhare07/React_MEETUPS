08fb833faf496d4be9d1766559c5f788e3c3171e
Sourav Khare <sourav.k@mantralabsglobal.com>
2023-06-19 09:57:03 +0530

    React Meet Ups App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetUps";
+import NewMeetupPage from "./pages/NewMeetUp";
+import Favorites from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" exact={true} element={<AllMeetupsPage />}></Route>
+        <Route path="/new-meetup" element={<NewMeetupPage />}></Route>
+        <Route path="/favorites" element={<Favorites />}></Route>
+      </Routes>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/pages/NewMeetUp.js
@@ -0,0 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-fd426-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      //navigating to home after submitting form
+      navigate("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
